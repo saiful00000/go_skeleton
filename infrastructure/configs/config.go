d2f757dd7ac5f8fc2de9b54660f017b7f59aaf73
shaiful <saiful161003@gmail.com>
2024-12-21 12:57:14 +0600

    Environment variable configuration added

--- infrastructure/configs/config.go
+++ infrastructure/configs/config.go
@@ -1,5 +1,11 @@
 package configs
 
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
 var LocalConfig *Config
 
 type Config struct {
@@ -8,4 +14,27 @@ type Config struct {
 	DbIp   string `mapstructure:"DBIP"`
 	DbName string `mapstructure:"DBNAME"`
 	Port   string `mapstructure:"PORT"`
+}
+
+func InitConfig() *Config {
+	viper.AddConfigPath(".")
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+    if err := viper.ReadConfig(); err != nil {
+        log.Fatal("Error while reading app.env file", err)
+    }
+
+    var config *Config
+
+    if err := viper.Unmarshal(&config); err != nil {
+        log.Fatal("Error reading app.env file", err)
+    }
+
+    return config
+}
+
+func SetConfig() {
+    LocalConfig = InitConfig()
 }
